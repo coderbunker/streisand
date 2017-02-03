96b182541626ef338bc3d1b5a3aaa0971a4850a2
vrms <vrms@netcologne.de>
2017-02-03 11:56:21 +0800

    Update debian-prerequisites.sh

--- debian-prerequisites.sh
+++ debian-prerequisites.sh
@@ -6,16 +6,16 @@
 
 sudo apt-get update
 
-echo 'installing Python 2.7'
+echo '    installing Python 2.7'
 sudo apt-get install -y python2.7
 
-echo 'installing git'
+echo '    installing git'
 sudo apt-get install -y git
 
-echo 'Installing the pip package management system for Python'
+echo '    Installing the pip package management system for Python'
 sudo apt-get install -y python-paramiko python-pip python-pycurl python-dev build-essential
 
-echo 'Installing Ansible'
+echo '    Installing Ansible'
 sudo pip install -y ansible markupsafe
 
 exit
