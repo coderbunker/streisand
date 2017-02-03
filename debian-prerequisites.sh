cd4c1dd1ca3f9e3f6981445756f93c91462dada2
vrms <vrms@netcologne.de>
2017-02-03 11:55:28 +0800

    Update debian-prerequisites.sh
    added -y for `apt-get install` operations

--- debian-prerequisites.sh
+++ debian-prerequisites.sh
@@ -7,15 +7,15 @@
 sudo apt-get update
 
 echo 'installing Python 2.7'
-sudo apt-get install python2.7
+sudo apt-get install -y python2.7
 
 echo 'installing git'
-sudo apt-get install git
+sudo apt-get install -y git
 
 echo 'Installing the pip package management system for Python'
-sudo apt-get install python-paramiko python-pip python-pycurl python-dev build-essential
+sudo apt-get install -y python-paramiko python-pip python-pycurl python-dev build-essential
 
 echo 'Installing Ansible'
-sudo pip install ansible markupsafe
+sudo pip install -y ansible markupsafe
 
 exit
